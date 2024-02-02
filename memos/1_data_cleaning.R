ae1f42e03ffc37a5e19cbd94623f98250cf60c3b
louiseoh2025 <louiseoh2025@u.northwestern.edu>
2024-02-02 17:40:04 -0600

    2/1 5:39pm

---
+++ memos/1_data_cleaning.R
@@ -0,0 +1,35 @@
+# DATA CLEANING
+
+# load library
+library(tidyverse)
+library(lubridate)
+
+# load data
+# https://www.kaggle.com/datasets/yeganehbavafa/mental-health-in-the-pregnancy-during-the-covid-19
+pregnancy <- read_csv("data/pregnancy.csv") |> 
+  janitor::clean_names()
+
+# glimpse data
+glimpse(pregnancy)
+
+# filter all rows with missing values
+for(col in names(pregnancy)) {
+  pregnancy <- pregnancy[!is.na(pregnancy[[col]]), ]
+}
+
+# rename variables
+pregnancy <- pregnancy |> 
+  rename(postnatal_depression = edinburgh_postnatal_depression_scale,
+         delivery_date = delivery_date_converted_to_month_and_year)
+
+# change variable types
+for (col in names(pregnancy)) {
+  if (col != "delivery_date" && is.character(pregnancy[[col]])) {
+    pregnancy[[col]] <- factor(pregnancy[[col]])
+  }
+}
+
+pregnancy <- pregnancy |>  
+  mutate(delivery_date = dmy(paste("01", delivery_date)))
+
+
