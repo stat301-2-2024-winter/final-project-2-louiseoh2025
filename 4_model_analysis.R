066684cabc52ac877c871d490b1696c90003b5e3
louiseoh2025 <louiseoh2025@u.northwestern.edu>
2024-02-15 22:52:40 -0600

    2/15 10:52pm

--- 4_model_analysis.R
+++ 4_model_analysis.R
@@ -27,10 +27,27 @@ load(here("results/pregnancy_recipes.rda"))
 # collect metrics
 metric_table <- lm_fit_folds |> collect_metrics() |> mutate(model = "lm") |> 
   bind_rows(lasso_fit_folds |> collect_metrics() |> mutate(model = "lasso"),
-            ridge_fit_folds |> collect_metrics() |> mutate(model = "ridge"),
-            rf_fit_folds |> collect_metrics() |> mutate(model = "rf")) |> 
-  relocate(model, .before = 1)
+            ridge_fit_folds |> collect_metrics() |> mutate(model = "ridge")) |> 
+  relocate(model, .before = 1) 
 metric_table |> knitr::kable()
+
+# tuned models
+best_rf <- show_best(rf_tuned, metric = "rmse")[1,]
+select_best(rf_tuned, metric = "rmse")
+best_bt <- show_best(bt_tuned, metric = "rmse")[1,]
+select_best(bt_tuned, metric = "rmse")
+best_knn <- show_best(knn_tuned, metric = "rmse")[1,]
+select_best(knn_tuned, metric = "rmse")
+best_rmse <- tibble(
+  model = c("RF", "KNN", "BT"),
+  RMSE = c(best_rf$mean, best_knn$mean, best_bt$mean),
+  std_err = c(best_rf$std_err, best_knn$std_err, best_bt$std_err),
+  n = c(best_rf$n, best_knn$n, best_bt$n)
+)
+best_rmse |> knitr::kable()
+
+# RMSE table
+
 
 # within 10% of the original price (similar to a confidence interval)
 lm_percent <- pregnancy_test |> 
